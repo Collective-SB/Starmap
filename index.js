090de12b3c0cf2f4ea3c7e2d206715d7e848ef58
Strikeeaglechase <Strikeeaglechase@gmail.com>
2020-12-05 22:22:58 -0500

    test5

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/favicon.ico", (req, res) => {
 		root: __dirname,
 	});
 });
-app.get("/test4.html", (req, res) => {
+app.get("/test5.html", (req, res) => {
 	res.sendFile("./test.html", {
 		root: __dirname,
 	});
